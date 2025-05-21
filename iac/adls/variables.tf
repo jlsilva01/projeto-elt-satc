c4d18eefccb66a03b17a7d4f9d11c6f9c32da9d4
Jorge Luiz da Silva <jlsilva01@outlook.com>
2025-05-20 23:52:00 -0300

    update files and folders (sanitization)

--- iac/adls/variables.tf
+++ iac/adls/variables.tf
@@ -1,6 +1,6 @@
 # Lista de variaveis utilizadas nos arquivos de terraform
 variable "resource_group_name" {
-  default = "learn-f9d7cc8b-cb65-49c9-9e38-1f13a0af60fc"
+  default = "learn-88611d7f-eef0-40a0-a84a-0b014f5e833d"
 }
 
 variable "location" {
